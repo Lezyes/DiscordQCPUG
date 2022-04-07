99a260b65f8e7a4abd40ec59472b46df517f4745
Barak <18008770+Lezyes@users.noreply.github.com>
2022-04-07 13:36:59 +0300

    1st steps

---
+++ main.py
@@ -0,0 +1,221 @@
+import discord
+import logging
+from secret import token
+from guild_games_roles import guilds_roles
+from text_to_emojis import emoji_dict
+from functools import partial
+from random import randint
+from asyncio import TimeoutError
+
+logging.basicConfig(level=logging.WARNING)
+
+# intents = discord.Intents.all()
+
+
+intents = discord.Intents.none()
+
+intents.guilds = True
+intents.members = True
+intents.voice_states = True
+intents.messages = True
+intents.reactions = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    guilds = client.guilds
+    for guild in guilds:
+        print(guild)
+
+async def get_player_stats(player_tag):
+    url = f"https://quake-stats.bethesda.net/api/v2/Player/Stats?name={player_tag}"
+    r = requests.get(url)
+    player_data = r.content
+    return player_data
+
+@client.event
+async def on_reaction_add(reaction, user):
+    pass
+
+@client.event
+async def on_message(message):
+    # if starting pug
+    if message.content.startswith('$pickup') or message.content.startswith('$pu'):
+        team_balance_options = {"Random": shuffle_list}
+
+        def shuffle_list(l):
+            for i in range(len(l)*2):
+                r = randint(0, len(l)-1)
+                l.append(l.pop(r))
+            return l
+
+        def match_emoji(selection_dict, emoji_dict, add_all_option=False):
+            selection_select = {}
+            emoji_select = {}
+            text = ""
+            emoji_options = list(emoji_dict.values())
+            for i, selection in enumerate(selection_dict):
+                emoji = emoji_options[i]
+                selection_select[selection] = emoji
+                emoji_select[emoji] = selection
+                text += f"{selection}: {emoji}\n"
+            if add_all_option:
+                i += 1
+                selection = "All"
+                emoji = emoji_options[i]
+                selection_select[selection] = emoji
+                emoji_select[emoji] = selection
+                text += f"{selection}: {emoji}\n"
+            return text, selection_select, emoji_select
+
+        async def collect_reactions(reply, emoji_select):
+            # kinda slow?
+            reply = await reply.channel.fetch_message(reply.id)
+            picked_reactions = set()
+            picked_selection = set()
+            for reaction in reply.reactions:
+                if reaction.emoji != "👍":
+                    for user in await reaction.users().flatten():
+                        if message.author == user:
+                            picked_reactions.add(reaction.emoji)
+                            if emoji_select.get(reaction.emoji) == "All":
+                                picked_selection = set([emoji_select[emoji] for emoji in emoji_select if emoji_select[emoji] != "All"])
+                                break
+                            break
+            if not picked_selection:
+                picked_selection = set([emoji_select[emoji] for emoji in picked_reactions])
+            return picked_selection
+
+        async def send_selection_message(text, emoji_select, reference=None):
+            reply = await message.channel.send(text, reference=reference)
+            for emoji in emoji_select:
+                await reply.add_reaction(emoji)
+            return reply
+
+        def check_reaction(reaction, user, check_message, ok_reactions):
+            return user == message.author and reaction.message.id == check_message.id and str(reaction.emoji) in ok_reactions
+
+        def check_msg(msg, check_message):
+            return user == message.author and msg.reference.message_id == check_message.id
+
+        guild = message.guild
+        channel = message.channel
+        roles = guilds_roles.get(guild.id, {}).get("roles")
+        channels = guilds_roles.get(guild.id, {}).get("channels")
+
+        # get players from channels
+        players = {}
+        for role in guild.roles:
+            if role.id in roles:
+                players[role.name] = role.members
+        for guild_channel in guild.channels:
+            if guild_channel.id in channels:
+                players[guild_channel.name] = guild_channel.members
+
+        # select source
+        text = "Please Choose Player source:\n"
+        selection_text, source_select, emoji_select = match_emoji(players, emoji_dict, add_all_option=True)
+        text += selection_text
+        source_reply = await send_selection_message(text, emoji_select, reference=message)
+        check_source = partial(check_reaction, check_message=source_reply, ok_reactions=emoji_select)
+        try:
+            reaction, user = await client.wait_for('reaction_add', timeout=600.0, check=check_source)
+        except TimeoutError:
+            await channel.send('You waited too long, please try again :D ')
+            retrun 0
+        source = emoji_select[str(reaction.emoji)]
+
+        # collect players from source
+        pu_players = set()
+        if source == "All":
+            for s in players:
+                for player in players[s]:
+                    pu_players.add(player)
+        else:
+            pu_players = set([player for player in players[source]])
+
+        # select players
+        text = "Please Choose Players:\n"
+        selection_text, player_select, emoji_select = match_emoji(pu_players, emoji_dict, add_all_option=True)
+        text += selection_text
+        text += f"Add players manually: ✍️\n"
+        text += f"Finished: 👍\n"
+
+        players_reply = await send_selection_message(text, emoji_select, reference=message)
+        emoji_select["✍️"] = "Add players manually"
+        await players_reply.add_reaction("✍️")
+        await players_reply.add_reaction("👍")
+        check_players = partial(check_reaction, check_message=players_reply, ok_reactions=["👍"])
+        try:
+            reaction, user = await client.wait_for('reaction_add', timeout=600.0, check=check_players)
+        except TimeoutError:
+            await channel.send('You waited too long, please try again :D ')
+            return 0
+
+        # collect selected players
+        picked_players_from_reply = await collect_reactions(players_reply, emoji_select)
+        picked_players = {p.display_name for p in picked_players_from_reply if p != "Add players manually"}
+        if "Add players manually" in  picked_players_from_reply:
+            text = "To add players, type their names separated by ,without spaces(example: name1,name2,name3) as a reply to this message\n"
+            add_reply = await send_selection_message(text, [], reference=message)
+            check_msg = partial(check_msg, check_message=add_reply)
+            try:
+                msg = await client.wait_for('message', timeout=600.0, check=check_msg)
+            except TimeoutError:
+                await channel.send('You waited too long, please try again :D ')
+                return 0
+            for p in msg.content.split(","):
+                picked_players.add(p)
+
+        # remove players
+        while len(picked_players) > 8:
+            text = 'You picked more than 8 players, please remove the extra fat:\n'
+            selection_text, player_select, emoji_select = match_emoji(picked_players, emoji_dict, add_all_option=False)
+            text += selection_text
+            text += f"Finished: 👍\n"
+
+            remove_players_reply = await send_selection_message(text, emoji_select, reference=message)
+            await remove_players_reply.add_reaction("👍")
+            check_remove_players = partial(check_reaction, check_message=remove_players_reply, ok_reactions=["👍"])
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout=600.0, check=check_remove_players)
+            except TimeoutError:
+                await channel.send('You waited too long, please try again :D ')
+                return 0
+            picked_players_to_remove_from_reply = await collect_reactions(players_reply, emoji_select)
+            picked_players_to_remove = {p.display_name for p in picked_players_to_remove_from_reply}
+            picked_players = {p for p in picked_players if p not in picked_players_to_remove}
+
+        if len(team_balance_options) > 1:
+            text = 'How do you wish to balance teams?\n'
+            selection_text, balance_select, emoji_select = match_emoji(team_balance_options, emoji_dict, add_all_option=False)
+            text += selection_text
+
+            team_balance_reply = await send_selection_message(text, emoji_select, reference=message)
+            check_team_balance = partial(check_reaction, check_message=team_balance_reply, ok_reactions=emoji_select)
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout=600.0, check=check_team_balance)
+            except TimeoutError:
+                await channel.send('You waited too long, please try again :D ')
+                return 0
+            balance_func = team_balance_options[emoji_select[str(reaction.emoji)]]
+        else:
+            balance_func = list(team_balance_options.values())[0]
+
+        picked_players = balance_func(list(picked_players))
+        team1 = []
+        team2 = []
+        for i,p in enumerate(picked_players):
+            if i>=len(picked_players)//2:
+                team2.append(p)
+            else:
+                team1.append(p)
+        team1 = ",".join(team1)
+        team2 = ",".join(team2)
+        await channel.send(f"Team 1:{team1}\nTeam 2:{team2} ")
+
+
+client.run(token)
