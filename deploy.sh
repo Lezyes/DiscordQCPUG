0cb4ad9c837d3134cab15ff7ca010f38695b40ef
Lezyes <lezyes@gmail.com>
2022-12-15 23:04:06 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-# docker network create qcbot-net
+docker network create qcbot-net
 
-# docker run --net qcbot-net -d --name redis-qc -p 6379:6379 -v /home/barakor/gits/DiscordQCPUG/redis_qc_data/:/data redis/redis-stack-server:latest 
+docker run --net qcbot-net -d --name redis-qc -p 6379:6379 -v /home/barakor/gits/DiscordQCPUG/redis_qc_data/:/data redis/redis-stack-server:latest 
 
-# docker build -t qcpug:test ./ 
-docker run -v /home/barakor/gits/DiscordQCPUG/:/code -it --rm -v /home/barakor/Downloads:/ocr  --net qcbot-net --name qcbot qcpug:test
+docker build -t qcpug:latest ./ 
+docker run -v /home/barakor/gits/DiscordQCPUG/:/code -it --rm -p 6443:443  --net qcbot-net --name qcbot qcpug:latest
