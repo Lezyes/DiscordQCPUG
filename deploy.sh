ccdad3dad1a467e4287c6a0c820363fce4790edb
Barak <18008770+Lezyes@users.noreply.github.com>
2022-12-20 17:32:27 +0200

    fixed deploy script, I think

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker network create qcbot-net
 
 docker run --net qcbot-net -d --name redis-qc -p 6379:6379 -v /home/barakor/gits/DiscordQCPUG/redis_qc_data/:/data redis/redis-stack-server:latest 
 
-docker rm --force qcbot ; docker build -t qcpug:test ./ ;docker run -d --net qcbot-net --name qcbot qcpug:test
+docker rm --force qcbot ; docker build -t qcbot:test ./ ;docker run -d --net qcbot-net --name qcbot qcbot:test
