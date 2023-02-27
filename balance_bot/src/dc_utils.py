23c69cc6b5fb761def7177c01c15c935f12a76cb
Barak <18008770+Lezyes@users.noreply.github.com>
2023-02-27 19:04:30 +0200

    Update dc_utils.py

--- balance_bot/src/dc_utils.py
+++ balance_bot/src/dc_utils.py
@@ -3,3 +3,7 @@ async def clean_up_msg(data_dict):
     if data_dict["clean_up"]:
         await data_dict["thread"].delete()
         await data_dict["message"].delete()
+
+async def get_thread_state(thread):
+    pass
+    # instaed of carrying all the data in data dict, data will be collected from the thread 
